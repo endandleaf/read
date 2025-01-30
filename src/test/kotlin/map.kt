d3c37199815487c24b6fce47440278944fde79f0
q9uo11 <q9uo11@qq.com>
2025-01-31 01:37:52 +0800

    first commit

--- src/test/kotlin/map.kt
+++
@@ -1,8 +0,0 @@
-
-
-fun main(){
-    var ma1= mutableMapOf("key1" to "value1", "key2" to "value1")
-    var ma2= mutableMapOf("key1" to "value2", "key2" to "value2")
-    ma1.putAll(ma2)
-    println(ma1.toString())
-}
